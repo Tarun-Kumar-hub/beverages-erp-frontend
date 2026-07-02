c77f7a4c4dc11a0f97f758a8ed420349d8cb1c0d
tarun <ctarun301@gmail.com>
2026-07-02 14:36:16 +0530

    Fix timezone formatting for date and time display

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,15 +1,44 @@
-import { formatDistanceToNow } from "date-fns";
+// import { formatDistanceToNow } from "date-fns";
 
-// Convert UTC to Indian Time
+// // Convert UTC to Indian Time
+
+// export const formatDateTime = (date) => {
+//   if (!date) return "-";
+
+//   const utcDate = new Date(date);
+
+//   const istDate = new Date(utcDate.getTime() + 5.5 * 60 * 60 * 1000);
+
+//   return istDate.toLocaleString("en-IN", {
+//     day: "numeric",
+//     month: "short",
+//     year: "numeric",
+//     hour: "numeric",
+//     minute: "2-digit",
+//     second: "2-digit",
+//     hour12: true,
+//   });
+// };
+
+// export const timeAgo = (date) => {
+//   if (!date) return "-";
+
+//   const utcDate = new Date(date);
+
+//   const istDate = new Date(utcDate.getTime() + 5.5 * 60 * 60 * 1000);
+
+//   return formatDistanceToNow(istDate, {
+//     addSuffix: true,
+//   });
+// };
+
+import { formatDistanceToNow } from "date-fns";
 
 export const formatDateTime = (date) => {
   if (!date) return "-";
 
-  const utcDate = new Date(date);
-
-  const istDate = new Date(utcDate.getTime() + 5.5 * 60 * 60 * 1000);
-
-  return istDate.toLocaleString("en-IN", {
+  return new Date(date).toLocaleString("en-IN", {
+    timeZone: "Asia/Kolkata",
     day: "numeric",
     month: "short",
     year: "numeric",
@@ -23,11 +52,7 @@ export const formatDateTime = (date) => {
 export const timeAgo = (date) => {
   if (!date) return "-";
 
-  const utcDate = new Date(date);
-
-  const istDate = new Date(utcDate.getTime() + 5.5 * 60 * 60 * 1000);
-
-  return formatDistanceToNow(istDate, {
+  return formatDistanceToNow(new Date(date), {
     addSuffix: true,
   });
 };
